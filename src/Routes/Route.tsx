5487177f4572f89f4aa027ef549158ed17875125
WPBASILAHMAD <WPBASILAHMAD>
2024-07-25 11:57:08 +0500

    All the missing assets added

--- src/Routes/Route.tsx
+++ src/Routes/Route.tsx
@@ -1,7 +1,11 @@
 import SamplePage from "../Pages/Pages/SamplePage";
 import { ProductPage } from "../Pages/Products";
-
+import Default from "../Pages/Dashboard/Default";
 export const routes = [
+  {
+    path: `${process.env.PUBLIC_URL}/dashboard`,
+    element: <Default />,
+  },
   {
     path: `${process.env.PUBLIC_URL}/sample_page`,
     element: <SamplePage />,
