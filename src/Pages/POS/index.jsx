0b846b6532759222127e42849ee8922125cc84ce
Najeeb <ceo@najeebmedia.com>
2024-07-26 22:37:22 +0500

    cart pay being ready

--- src/Pages/POS/index.jsx
+++ src/Pages/POS/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import POSItems from "./POSItems"; // Ensure the correct path to POSItems
 import "./pos.css"; // Ensure the CSS file is imported
 import { POSCart } from "./Cart";
+import PayCart from "./Pay";
 
 export const POSHome = () => {
   return (
     <div className="page-body pos-container">
       <div className="product-area">
-        <POSItems />
+        <PayCart payableAmount={250} />
       </div>
       <div className="cart-area">
         <POSCart />
