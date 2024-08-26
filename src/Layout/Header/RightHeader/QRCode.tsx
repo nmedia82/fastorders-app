63242d826742de00834781ec2986230130d51e93
WPBASILAHMAD <basilmedia7@gmail,com>
2024-08-26 05:56:49 -0700

    QR code Icon Added

--- src/Layout/Header/RightHeader/QRCode.tsx
+++ src/Layout/Header/RightHeader/QRCode.tsx
@@ -1,20 +1,23 @@
 /** @format */
 
 import React from "react";
-import { Input, InputGroup, InputGroupText } from "reactstrap";
-import { Badges, H6, Image, LI, P, UL } from "../../../AbstractElements";
-import SvgIcon from "../../../Utils/CommonComponents/CommonIcons/CommonSvgIcons";
-import { dynamicImage } from "../../../Utils";
-import { JaneCooper } from "../../../Utils/Constants";
-import { chatHeaderData } from "../../../Data/Layout/RightHeader";
 import { Link } from "react-router-dom";
-
+import { LI } from "../../../AbstractElements";
+import SvgIcon from "../../../Utils/CommonComponents/CommonIcons/CommonSvgIcons";
+import { Image } from "../../../AbstractElements";
+import { dynamicImage } from "../../../Utils";
 export default function QRCodePage() {
   return (
     <LI className="cart-nav onhover-dropdown">
       <div>
         <Link to={"/qrcode"}>
-          <SvgIcon iconId="book" />
+          {/* <SvgIcon iconId="qr-code" /> */}
+          <Image
+            className="img-fluid"
+            style={{ width: "20px", height: "20px" }}
+            src={dynamicImage(`qrcode/qrcode.png`)}
+            alt=""
+          />
         </Link>
       </div>
     </LI>
